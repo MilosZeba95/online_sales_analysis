0d3040741b16be2e4bfc16832c4150287015f21a
Milos Zeba <miloszeba306@gmail.com>
2025-02-14 14:03:37 +0100

    Changes to main.py before merging with add-cart-functionality branch!

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ if __name__ == "__main__":
     manager = ProductManager()
     
     #Adding products
-    product1 = Product("Laptop",852,12)
-    product2 = Product("Monitor",200,5)
-    product3 = Product("Keyboard",70,10)
-    product4 = Product("Mouse",120,9)
+    product1 = Product("Laptop",852,7)
+    product2 = Product("Monitor",200,8)
+    product3 = Product("Keyboard",70,3)
+    product4 = Product("Mouse",120,12)
 
     #Displaying all products
     manager.add_product(product1)
@@ -16,10 +16,4 @@ if __name__ == "__main__":
     manager.add_product(product4)
     
     
-    #Displaying all products
-    print("\nList of all products")
-    manager.display_all_products()
-    
-    #Display total value of inventory
-    print("\nTotal value of inventory")
-    manager.total_value()
+ 
